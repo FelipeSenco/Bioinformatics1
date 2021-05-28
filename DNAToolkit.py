fba7927bb41f1bdbb904f0f1c8274638fcd03bca
Felipe de Senco <55833207+Dijoig@users.noreply.github.com>
2021-05-28 10:31:50 +0100

    part 3 and 4

--- DNAToolkit.py
+++ DNAToolkit.py
@@ -1,27 +1,64 @@
-#DNA Toolkit:
+# DNA Toolkit:
+from typing import Counter
 from structures import *
 
-#Check the sequence to make sure it is a DNA String:
+# Check the sequence to make sure it is a DNA String:
 def validateSeq(dna_seq):
-    tmpseq  = dna_seq.upper()
+    tmpseq = dna_seq.upper()
     for nuc in tmpseq:
         if nuc not in Nucleotides:
             return False
     return tmpseq
 
-#Count nucleotides in a DNA sequence:
+# Count nucleotides in a DNA sequence:
 def countNucFrequency(seq):
-    tmpFreqDict = {"A": 0,"C": 0,"G": 0,"T": 0}
+    tmpFreqDict = {"A": 0, "C": 0, "G": 0, "T": 0}
     for nuc in seq:
-       tmpFreqDict[nuc] += 1
+        tmpFreqDict[nuc] += 1
     return tmpFreqDict
 
-#Transcript DNa sequence to RNA:
+# Transcript DNa sequence to RNA:
 def transcription(seq):
     """DNA -> RNA transcription. Replacing Thymine with Uracil"""
     return seq.replace("T", "U")
 
-#Getting reverse complement DNA sequence:
+# Getting reverse complement DNA sequence:
 def reverse_complement(seq):
     """Swapping adenine with thymine and guanine with cytosine. Reversing newly generated string"""
-    return ''.join([DNA_ReverseComplement[nuc] for nuc in seq])[::-1]
+    # return ''.join([DNA_ReverseComplement[nuc] for nuc in seq])[::-1]
+    # or
+    # Pythonic approach. A little bit faster solution.
+    mapping = str.maketrans('ATCG', 'TAGC')
+    return seq.translate(mapping)[::-1]
+
+# Guanine and Cytocine content calculation:
+def gc_content(seq):
+    """GC Content in a DNA/RNA sequence"""
+    return round((seq.count('C') + seq.count('G')) / len(seq) * 100)
+
+def gc_content_subsec(seq, k=20):
+    """GC Content in a DNA/RNA sub-sequence length k. k=20 by default"""
+    res = []
+    for i in range(0, len(seq) - k + 1, k):
+        subseq = seq[i: i + k]
+        res.append(gc_content(subseq))
+    return res
+
+#translation function:
+def translate_seq(seq, init_pos=0):
+    """Translate  a DNA sequence into an aminoacid sequence"""
+    return [DNA_Codons[seq[pos: pos + 3]] for pos in range(init_pos, len(seq) - 2, 3)]
+
+#Condon usage function:
+def condon_usage(seq, aminoacid):
+    """Provides the frequency of each condon encoding a given aminoacid in a DNA sequence"""
+    tmpList = []
+    for i in range(0, len(seq) - 2, 3):
+        if DNA_Codons[seq[i: i + 3]] == aminoacid:
+            tmpList.append(seq[i: i + 3])
+
+    freqDict = dict(Counter(tmpList))
+    totalWight = sum(freqDict.values())
+    for codon in freqDict:
+        freqDict[codon] = round(freqDict[codon] / totalWight, 2)
+    return freqDict
--- main.py
+++ main.py
@@ -11,11 +11,18 @@ rndDNAStr = ''.join([random.choice(Nucleotides)
 DNAStr = validateSeq(rndDNAStr)
 RNAStr = transcription(DNAStr)
 
-print(f'\nSequence: {colored(DNAStr)}\n')
+print(f'\nSequence: {(DNAStr)}\n')
 print(f'[1] + Sequence length: {len(DNAStr)}\n')
 print(f'[2] + Nucleotide frequency: {countNucFrequency(DNAStr)}\n')
-print(f'[3] + DNA/RNA transcription: {colored(RNAStr)}\n')
+print(f'[3] + DNA/RNA transcription: {(RNAStr)}\n')
 
-print(f"[4] + DNA String + Receverse Complement:\n5' {(DNAStr)} 3'")
+print(f"[4] + DNA String + Complement + Receverse Complement:\n5' {(DNAStr)} 3'")
 print(f"   {''.join(['|' for c in range(len(DNAStr))])}")
-print(f"3' {(reverse_complement(DNAStr))} 5'\n")
+print(f"3' {(reverse_complement(DNAStr)[::-1])} 5' [Complement]")
+print(f"5' {(reverse_complement(DNAStr))} 3' [Rev. Complement]\n")
+
+print(f'[5] + GC Content: {gc_content(DNAStr)}%\n')
+print(f'[6] + GC Content in Subsection k=5: {gc_content_subsec(DNAStr, k=5)}\n')
+
+print(f'[7] + Aminoacids Sequence from DNA: {translate_seq(DNAStr, 0)}\n')
+print(f"[8] + Condon frequency (L): {condon_usage(DNAStr, 'L')}")
