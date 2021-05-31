2c8fbc99f59958d0323aea2e472e5e80880aa95b
Felipe de Senco <55833207+Dijoig@users.noreply.github.com>
2021-05-31 13:58:53 +0100

    PART 6 AND 7

--- DNAToolkit.py
+++ DNAToolkit.py
@@ -1,5 +1,5 @@
 # DNA Toolkit:
-from typing import Counter
+from typing import Counter, OrderedDict
 from structures import *
 
 # Check the sequence to make sure it is a DNA String:
@@ -62,3 +62,55 @@ def condon_usage(seq, aminoacid):
     for codon in freqDict:
         freqDict[codon] = round(freqDict[codon] / totalWight, 2)
     return freqDict
+
+#generate the reading frames:
+def gen_reading_frames(seq):
+    """Generate the six reading frames of a DNA sequence, including the reverse complement"""
+    frames = []
+    frames.append(translate_seq(seq, init_pos=0))
+    frames.append(translate_seq(seq, init_pos=1))
+    frames.append(translate_seq(seq, init_pos=2))
+    frames.append(translate_seq(reverse_complement(seq), init_pos=0))
+    frames.append(translate_seq(reverse_complement(seq), init_pos=1))
+    frames.append(translate_seq(reverse_complement(seq), init_pos=2))
+    return frames
+
+#scan possbile proteins for reading frames:
+def proteins_from_rf(aa_seq):
+    """Compute all possible proteins in an aminoacid seq and return a list of possible proteins"""
+    current_prot = []
+    proteins = []
+    for aa in aa_seq:
+        if aa == "_":
+            #STOP accumulating aminoacids if _ - STOP was found
+            if current_prot:
+                for p in current_prot:
+                    proteins.append(p)
+                current_prot = []
+        else:
+            # START accumulating amino acids if M - START was found
+            if aa == "M":
+                current_prot.append("")
+            for i in range(len(current_prot)):
+                current_prot[i] += aa
+    return proteins
+
+#scan all possible proteins from a DNA sequence:
+def all_proteins_from_orfs(seq, startReadPos=0, endReadPos=0, ordered=False):
+    """Compute all possible proteins for all open reading frames"""
+    """Protein Seach DB: https://www.ncbi.nlm.nih.gov/nuccore/NM_001185097.2"""
+    """API can be used to pull protein info"""
+    if endReadPos > startReadPos:
+        rfs = gen_reading_frames(seq[startReadPos: endReadPos])
+    else:
+        rfs = gen_reading_frames(seq)
+    
+    res = []
+    for rf in rfs:
+        prots = proteins_from_rf(rf)
+        for p in prots:
+            res.append(p)
+
+    if ordered:
+        return sorted(res, key=len, reverse=True)
+    return res
--- main.py
+++ main.py
@@ -26,3 +26,11 @@ print(f'[6] + GC Content in Subsection k=5: {gc_content_subsec(DNAStr, k=5)}\n')
 
 print(f'[7] + Aminoacids Sequence from DNA: {translate_seq(DNAStr, 0)}\n')
 print(f"[8] + Condon frequency (L): {condon_usage(DNAStr, 'L')}")
+
+print("[9] + Reading_frames:")
+for frame in gen_reading_frames(DNAStr):
+    print(frame)
+
+print("\n[10] + All proteins in 6 open reading frames:")
+for prot in all_proteins_from_orfs(DNAStr, startReadPos=0, endReadPos=0, ordered=True):
+    print(f'{prot}')
